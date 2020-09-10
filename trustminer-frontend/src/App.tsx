0ac949d238e905223973e1cb408b2f0dbeebcbcf
Denis <denis@koljada.de>
2020-09-11 01:15:05 +0200

    added miner unit test, reverted frontend login

--- trustminer-frontend/src/App.tsx
+++ trustminer-frontend/src/App.tsx
@@ -6,11 +6,11 @@ import {SnackbarProvider} from "notistack";
 import FrontPage from "./pages/FrontPage";
 import Dashboard from "./pages/Dashboard";
 import SignIn from "./pages/SignIn";
-import {Redirect, Route, Switch, useLocation} from 'react-router-dom';
+import {Route, Switch, useLocation} from 'react-router-dom';
 import Introduction from "./pages/Introduction";
 import NotFound from "./pages/NotFound";
 import {saveEvent} from "./util/survey_util";
-import {LOGGED_IN, TYPE_NAVIGATE} from "./util/constants";
+import {TYPE_NAVIGATE} from "./util/constants";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import Impressum from "./pages/Impressum";
 
@@ -21,31 +21,24 @@ function App() {
         saveEvent("location", TYPE_NAVIGATE, location.pathname)
     }, [location])
 
-    const loggedIn = localStorage.getItem(LOGGED_IN) === "true"
-
     return (
         <ThemeProvider theme={mainTheme}>
             <SnackbarProvider maxSnack={4}>
                 <main>
-                    {loggedIn ?
-                        <Switch>
-                            <Route exact path="/" component={FrontPage}/>
-                            <Route exact path="/introduction" component={Introduction}/>
-                            <Route exact path="/analysis" component={Dashboard}/>
-                            <Route exact path="/modeler" component={Dashboard}/>
-                            <Route exact path="/trust-policies" component={Dashboard}/>
-                            <Route exact path="/uncertainty-list" component={Dashboard}/>
-                            <Route exact path="/settings" component={Dashboard}/>
-                            <Route exact path="/about" component={Dashboard}/>
-                            <Route exact path="/privacy-policy" component={PrivacyPolicy}/>
-                            <Route exact path="/impressum" component={Impressum}/>
-                            <Route component={NotFound}/>
-                        </Switch> :
-                        <Switch>
-                            <Route exact path="/login" component={SignIn}/>
-                            <Redirect to="/login"/>
-                        </Switch>
-                    }
+                    <Switch>
+                        <Route exact path="/" component={FrontPage}/>
+                        <Route exact path="/introduction" component={Introduction}/>
+                        <Route exact path="/analysis" component={Dashboard}/>
+                        <Route exact path="/modeler" component={Dashboard}/>
+                        <Route exact path="/trust-policies" component={Dashboard}/>
+                        <Route exact path="/uncertainty-list" component={Dashboard}/>
+                        <Route exact path="/settings" component={Dashboard}/>
+                        <Route exact path="/about" component={Dashboard}/>
+                        <Route exact path="/login" component={SignIn}/>
+                        <Route exact path="/privacy-policy" component={PrivacyPolicy}/>
+                        <Route exact path="/impressum" component={Impressum}/>
+                        <Route component={NotFound}/>
+                    </Switch>
                 </main>
             </SnackbarProvider>
         </ThemeProvider>
